72ea7f6bcc5ff4384758de742e28cf008e93e169
jhonatan <jhonnathanavendano4@gmail.com>
2024-06-27 08:48:50 -0500

    Encapsulamiento -Implementación de Clase Persona por [jhonnathan]

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,11 @@
 
+import java.time.LocalDate;
+
+public class Persona {
+    // Propiedades privadas
+    private String nombre;
+    private int edad;
+    private LocalDate fechaDeNacimiento;
+}
+
 
