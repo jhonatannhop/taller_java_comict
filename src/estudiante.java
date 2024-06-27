ce3c32f309172333c2b4bb919f176d14b3a640d4
jhonatan <jhonnathanavendano4@gmail.com>
2024-06-27 09:18:32 -0500

    Herencia -Implementación de Constructores por [jhonnathan]

--- src/estudiante.java
+++ src/estudiante.java
@@ -1,4 +1,22 @@
 public class estudiante extends Persona {
 
+    private String grado;
 
+    public Estudiante(String nombre, int edad, String grado) {
+        super(nombre, edad);
+        this.grado = grado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Grado: " + grado;
+    }
+    public String toString() {
+        return "Nombre: " + nombre + ", Edad: " + edad + ", Grado: " + grado;
+    }
+    public Estudiante(String nombre, int edad, String grado) {
+        super(nombre, edad);  // Llama al constructor de la clase base (Persona)
+        this.grado = grado;   // Inicializa la propiedad propia (grado)
+    }
 }
+
